72585b0c5b4d8b3d0b9eb8b084163bc09f9d741b
Jennifer Lopez <jenniferlopez@ucsb.edu>
2025-11-03 22:33:03 -0800

    jl - removed api keys from code and moved them to env file

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,13 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAphIzwHuLG_LqE7nv5yuHkwpVLTxQa7AQ",
-  authDomain: "recallmd-83caf.firebaseapp.com",
-  projectId: "recallmd-83caf",
-  storageBucket: "recallmd-83caf.firebasestorage.app",
-  messagingSenderId: "218440428095",
-  appId: "1:218440428095:web:1b80fb8ed8279ed951a68a",
-  measurementId: "G-L4PPFQTLX0"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
